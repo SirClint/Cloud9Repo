8e1e476ec0fe67631d052e457659353a580a2a95
Clint <Clint_R_Smith@yahoo.com>
2020-04-22 01:02:45 +0000

    [CRS] Adding Hello.java

---
+++ Hello.java
@@ -0,0 +1,11 @@
+/**
+ * Class Hello
+ */
+public class Hello {
+	public static void main(String[] args) {
+		
+    	System.out.println("Hello from AWS Cloud9!");
+    	System.out.println("Java version: " + System.getProperty("java.version"));
+    	test
+	}
+}
